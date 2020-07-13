3d0e315e55d25ea068952b050bb1af67234ad105
xMrShadyx <daredevil91138@gmail.com>
2020-07-13 16:17:12 +0300

    Updated Nested_Loops_Lab/07_Cinema_Tickets.py

--- Programming Basic/Nested_Loops_Lab/07_Cinema_Tickets.py
+++ Programming Basic/Nested_Loops_Lab/07_Cinema_Tickets.py
@@ -1,39 +1,26 @@
 line = input()
-student_tickets = 0
-standard_tickets = 0
-kid_tickets = 0
-total_tickets = 0
-
-while line != 'Finish':
-    available_seats = int(input())
-    movie_tickets_sold = 0
-
-    ticket_type = input()
-    while ticket_type != 'End':
-        movie_tickets_sold += 1
-        total_tickets += 1
-        if ticket_type == 'students':
-            student_tickets += 1
-        elif ticket_type == 'standard':
-            standard_tickets += 1
-        else:
-            kid_tickets += 1
-
-        if movie_tickets_sold == available_seats:
+standard_ticket = 0
+student_ticket = 0
+kid_ticket = 0
+total_sold_tickets = 0
+while line != "Finish":
+    free_sits = int(input())
+    full_sits = 0
+    while full_sits < free_sits:
+        type_ticket = input()
+        if type_ticket == 'standard':
+            standard_ticket += 1
+        elif type_ticket == 'student':
+            student_ticket += 1
+        elif type_ticket == 'kid':
+            kid_ticket += 1
+        if type_ticket == 'End':
             break
-        ticket_type = input()
-
-    percent = movie_tickets_sold / available_seats * 100
-    print(f'{line} - {percent:.2f}% full.')
+        full_sits += 1
+        total_sold_tickets += 1
+    print(f'{line} - {full_sits / free_sits * 100:.2f}% full.')
     line = input()
-
-print(f"Total tickets: {total_tickets}")
-
-percent_student = student_tickets / total_tickets * 100
-print(f"{percent_student:.2f}% student tickets.")
-
-percent_standard = standard_tickets / total_tickets * 100
-print(f"{percent_standard:.2f}% standard tickets.")
-
-percent_kids = kid_tickets / total_tickets * 100
-print(f"{percent_kids:.2f}% kids tickets.")
+print(f'Total tickets: {total_sold_tickets}')
+print(f'{student_ticket / total_sold_tickets * 100:.2f}% student tickets.')
+print(f'{standard_ticket / total_sold_tickets * 100:.2f}% standard tickets.')
+print(f'{kid_ticket / total_sold_tickets * 100:.2f}% kids tickets.')
