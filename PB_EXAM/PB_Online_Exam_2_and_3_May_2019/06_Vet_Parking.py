3f81469aeecde2ff8e02599de1b3b2f6c5534498
xMrShadyx <daredevil91138@gmail.com>
2020-09-01 16:06:58 +0300

    06_Vet_Parking

--- PB_EXAM/PB_Online_Exam_2_and_3_May_2019/06_Vet_Parking.py
+++ PB_EXAM/PB_Online_Exam_2_and_3_May_2019/06_Vet_Parking.py
@@ -1,3 +1,19 @@
 amount_days = int(input())
 amount_hours = int(input())
+price = 0
+total_price = 0
 
+for i in range(amount_days):
+    price = 0
+    for k in range(amount_hours):
+        if i % 2 == 0 and k % 2 != 0:
+            price += 1.25
+            total_price += 1.25
+        elif i % 2 != 0 and k % 2 == 0:
+            price += 2.5
+            total_price += 2.5
+        else:
+            price += 1
+            total_price += 1
+    print(f"Day: {i + 1} - {price:.2f} leva")
+print(f"Total: {total_price:.2f} leva")
