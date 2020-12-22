78d5b79769e0087e6814a061d7ceee9ad66b1744
xMrShadyx <daredevil91138@gmail.com>
2020-12-22 09:44:37 +0200

    C# Adv Statement Lab

---
+++ Programming Basic C#/conditional_statement_adv_lab/invalid_number_10/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace invalid_number_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+
+            switch (number)
+            {
+                case 0:
+                    break;
+                default:
+                    if (number < 100)
+                    {
+                        Console.WriteLine("invalid");
+                    } else if (number > 200)
+                    {
+                        Console.WriteLine("invalid");
+                    }
+                    break;
+                    
+            }
+            
+        }
+    }
+}
---
+++ Programming Basic C#/conditional_statement_adv_lab/small_shop_05/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace small_shop_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string article = Console.ReadLine();
+            string town = Console.ReadLine();
+            double amount = double.Parse(Console.ReadLine());
+            double price = 0;
+
+            switch (town)
+            {
+                case "Sofia":
+                    switch (article)
+                    {
+                        case "coffee":
+                            price = 0.5;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "water":
+                            price = 0.8;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "beer":
+                            price = 1.2;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "sweets":
+                            price = 1.45;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "peanuts":
+                            price = 1.6;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                    }
+                    break;
+                case "Plovdiv":
+                    switch (article)
+                    {
+                        case "coffee":
+                            price = 0.4;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "water":
+                            price = 0.7;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "beer":
+                            price = 1.15;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "sweets":
+                            price = 1.30;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "peanuts":
+                            price = 1.5;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                    }
+                    break;
+                case "Varna":
+                    switch (article)
+                    {
+                        case "coffee":
+                            price = 0.45;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "water":
+                            price = 0.70;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "beer":
+                            price = 1.10;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "sweets":
+                            price = 1.35;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                        case "peanuts":
+                            price = 1.55;
+                            Console.WriteLine("{0:0.00}", price * amount);
+                            break;
+                    }
+                    break;
+            }
+            
+            
+        }
+    }
+}
